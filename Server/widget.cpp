c70b90aea001154bd8d90b16cb9c40323c4c1e77
zheyudu39-del <zheyudu39@gmail.com>
2026-04-05 17:55:27 +0800

    Add Server module and reorganize project structure

---
+++ Server/widget.cpp
@@ -0,0 +1,27 @@
+#include "widget.h"
+#include "./ui_widget.h"
+
+Widget::Widget(QWidget *parent)
+    : QWidget(parent)
+    , ui(new Ui::Widget)
+{
+    ui->setupUi(this);
+    server=new QTcpServer;
+    server->listen(QHostAddress::AnyIPv4,8000);
+    //客户端发起连接
+    connect(server,&QTcpServer::newConnection,this,&Widget::newClienthander);
+}
+
+Widget::~Widget()
+{
+    delete ui;
+}
+void Widget::newClienthander()
+{
+    //建立客户连接
+    QTcpSocket *socket=server->nextPendingConnection();
+    socket->peerAddress();//获取地址
+    socket->peerPort();//获取端口号
+    ui->iplineedit->setText(socket->peerAddress().toString());
+    ui->portlineedit->setText(QString::number(socket->peerPort()));
+}
